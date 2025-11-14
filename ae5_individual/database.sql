2dcfd6be69b17fb2d731d5035d5a4b2c1ccac6b1
Cristian Aranda <c.aranda.borquez@gmail.com>
2025-11-14 15:12:28 -0300

    Se agregan archivos del proyecto de nombre ae5_individual y una app´llamada registros_ORM // Se crea base de datos planteada en database.sql

---
+++ ae5_individual/database.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS ae5_individual;
+
+USE ae5_individual;
---
+++ ae5_individual/registros_ORM/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RegistrosOrmConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'registros_ORM'
